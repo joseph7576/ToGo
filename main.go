9a11763c0c2d5a304886233b7f93ab7155f72eeb
Joseph Hosseini <hosseini7576@outlook.com>
2024-11-26 20:20:43 +0330

    feat(main): add task struct + some testing

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type task struct {
+	content string
+	isDone  bool
+	created time.Time
+}
+
+func (t task) String() string {
+	return fmt.Sprintf("|- Content: %s\n|- Is Done: %t\n|- Created: %s",
+		t.content,
+		t.isDone,
+		t.created.Format("Jan 2 2006 @ 3:4 PM"),
+	)
+}
 
 func main() {
-	fmt.Println("Hello World")
+	var tasksList []task
+
+	t1 := task{
+		"Test Content Task 1",
+		false,
+		time.Now(),
+	}
+
+	tasksList = append(tasksList, t1)
+
+	fmt.Println(tasksList)
+	fmt.Println(t1)
 }
