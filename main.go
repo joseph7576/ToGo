c93d0923f5ea13e01c927e48b23b323c8c1b2f57
Joseph Hosseini <hosseini7576@outlook.com>
2024-12-03 11:37:45 +0330

    fix(createTask): change json to txt and separate data with enter

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"os"
 	"time"
@@ -13,16 +14,16 @@ const (
 )
 
 type task struct {
-	content string
-	isDone  bool
-	created time.Time
+	Content string    `json:"content"`
+	IsDone  bool      `json:"isDone"`
+	Created time.Time `json:"created"`
 }
 
 func (t task) String() string {
 	return fmt.Sprintf(TaskStringerFormat,
-		t.content,
-		t.isDone,
-		t.created.Format(TaskTimeFormat),
+		t.Content,
+		t.IsDone,
+		t.Created.Format(TaskTimeFormat),
 	)
 }
 
@@ -47,9 +48,30 @@ func main() {
 
 func createTask(scanner *bufio.Scanner) {
 	newTask := task{
-		content: scanner.Text(),
-		isDone:  false,
-		created: time.Now(),
+		Content: scanner.Text(),
+		IsDone:  false,
+		Created: time.Now(),
+	}
+
+	file, err := os.OpenFile("taskDB.txt", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0777)
+	if err != nil {
+		fmt.Println("X Can't open or create file -> ", err)
+		return
+
+	}
+
+	defer file.Close()
+
+	data, err := json.Marshal(newTask)
+	if err != nil {
+		fmt.Println("X Can't marshal newTask -> ", err)
+	}
+
+	data = append(data, []byte("\n")...)
+
+	_, err = file.Write(data)
+	if err != nil {
+		fmt.Println("X Can't write to file -> ", err)
 	}
 
 	tasksList = append(tasksList, newTask)
